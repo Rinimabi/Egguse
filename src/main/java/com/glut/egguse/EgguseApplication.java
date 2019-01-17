147328e22b9cbc31637e929e4e1d0a02eb222eee
45321 <453211317@qq.com>
2019-01-17 14:48:37 +0800

    First Commit. Base complete.

---
+++ src/main/java/com/glut/egguse/EgguseApplication.java
@@ -0,0 +1,14 @@
+package com.glut.egguse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgguseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgguseApplication.class, args);
+    }
+
+}
+
