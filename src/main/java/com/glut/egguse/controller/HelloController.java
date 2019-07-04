9ea251773de219f51bfc87722335163328d77702
chriswei <453211317@qq.com>
2019-07-04 23:40:50 +0800

    增加测试接口。

--- src/main/java/com/glut/egguse/controller/HelloController.java
+++ src/main/java/com/glut/egguse/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.glut.egguse.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String hello(){
-        return "hello world.";
+    @GetMapping(value = "/helloGet")
+    public HashMap<String,String> helloGet(){
+        HashMap<String,String> map = new HashMap<>();
+        map.put("result","yes");
+        return map;
+    }
+    @PostMapping(value = "/helloPost")
+    public HashMap<String,String> helloPost(@RequestBody Map<String,String> data){
+        HashMap<String,String> map = new HashMap<>();
+        System.out.println("data:"+data.get("data"));
+        return map;
     }
 }
